7b57060555716b8c948b86fbbe14003eb19cc969
Phil Jacob <jack.joe1817@gmail.com>
2024-01-30 21:48:19 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+
 const header = document.querySelector("header");
 
 window.addEventListener("scroll",function()  {  
     header.classList.toggle ("sticky", window.scrollY > 0)
-
 });
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none';
+}
