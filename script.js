1a371c6d621099b101726bfafad9f1f7c9c64da4
Phil Jacob <jack.joe1817@gmail.com>
2024-02-01 02:55:14 +0530

    last commit

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 
-const header = document.querySelector("header");
-
-window.addEventListener("scroll",function()  {  
-    header.classList.toggle ("sticky", window.scrollY > 0)
-});
-
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex';
